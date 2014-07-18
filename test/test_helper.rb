ff4240af1003a9df3c6884f2a2a5b13ea2b940f2
Alex Kardos <alexander.kardos@gmail.com>
2014-07-17 23:28:28 -0600

    Added transactions to allow tests to run properly at ~> 2.0

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -69,3 +69,20 @@ class Minitest::Test
 
 end
 
+class MiniTest::Spec
+ 
+  def run( *args, &block )
+    value = nil
+ 
+    begin
+      ActiveRecord::Base.connection.transaction do
+        value = super
+        raise ActiveRecord::Rollback
+      end
+    rescue ActiveRecord::Rollback
+    end
+ 
+    return value  # The result of run must be always returned for the pretty dots to show up
+  end
+ 
+end
